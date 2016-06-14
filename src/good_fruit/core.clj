45cc1cce4a393a7c7d9856c6cfec5b3e0230a27b
John Stevenson <john@jr0cket.co.uk>
2016-06-14 23:34:42 +0100

    Funcitons to process grapes with thread macro
    defined the following functions to process grapes and return any
    non-rotten grapes after they have been cleaned.
    
    * split-cluster
    return the collection of grapes, given a grape cluster
    
    * not-rotten
    filter out grapes that are marked at rotten
    
    * clean-grape
    update a grape to be clean

--- src/good_fruit/core.clj
+++ src/good_fruit/core.clj
@@ -23,3 +23,68 @@
              {:rotten? false :clean? false}]}
    :colour :black])
 
+;; We want to split the grap clusters into individual grapes, discarding the rotten grapes.  The remaing grapes will be checked to see if they are clean.  We should be left with one green and one black grape.
+
+;; pass a cluster data structure like
+#_{:grapes [{:rotten? false :clean? false}
+          {:rotten? true  :clean? false}]
+ :colour :green}
+;; we only ask for the first key's value, so colour doent matter
+(defn split-cluster
+"Takes a grape cluster and returns the vector of all the grapes in that cluster"
+  [cluster]
+  (:grapes cluster))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; testing code in the REPL
+
+;; The grape-clusters data structure is a vector of two grape clusters.
+;; To see what a grape cluster is, get the first element of that data structure
+(first grape-clusters)
+;; => {:grapes [{:rotten? false, :clean? false} {:rotten? true, :clean? false}], :colour :green}
+
+;; For each cluster in grape-clusters, return just the :grapes data, ignoring the colour informtion
+(split-cluster {:grapes [{:rotten? false :clean? false}
+                         {:rotten? true  :clean? false}]
+                :colour :green})
+;; =>[{:rotten? false, :clean? false} {:rotten? true, :clean? false}]
+
+;; end of testing code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn not-rotten
+  "Given a grape, only return it if it is not rotten.  A grape is defined as {:rotten? true|false :clean? true|false}"
+  [grape]
+  (not (:rotten? grape)))
+
+
+(defn clean-grape
+  "Given a grape, updating the grapes :clean? value to true regardless of its current value.  A grape is defined as {:rotten? true|false :clean? true|false}"
+  [grape]
+  (assoc grape :clean? true))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; testing in REPL
+(clean-grape {:rotten? false :clean? false})
+;; => {:rotten? false, :clean? true}
+
+;; end of testing code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Functional composition using the thread last macro.
+;; Each line passes its evaluate value to the next line as its last argument
+
+;; Algorithm
+;; * evaluate the name grape-clusters and return the data structure it points to.
+;; * use mapcat to map the split-clusters function over each element in grape-clusters, returning 4 grapes concatinated into one collection
+;; * filter the 4 grapes, dropping the grapes where :rotten? equals true, returning 2 grapes
+;; * update each grape to have a :clean? value of true
+
+(->> grape-clusters
+     (mapcat split-cluster)
+     (filter not-rotten)
+     (map clean-grape))
+
+;; => ({:rotten? false, :clean? true} {:rotten? false, :clean? true})
+
