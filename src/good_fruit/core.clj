4f76cdd9273e1b6ae01a30aae84c4ab4738b6fd0
John Stevenson <john@jr0cket.co.uk>
2016-06-14 20:09:14 +0100

    A data structure for grape clusters & description
    Added a document header to describe the project, including copyright,
    license and original content source.
    
    Added a data structure to represent a cluster of grapes that we are
    going to process

--- src/good_fruit/core.clj
+++ src/good_fruit/core.clj
@@ -1,3 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fruity Transducers
+;;
+;; Exploring transducers at the uSwitch Clojure Dojo June 2016
+;;
+;; Copyright (C) 2016 by jr0cket et al.
+;; License: Creative Commons Attribution Share-Alike 4.0 International
+;; Original content: https://funcool.github.io/clojurescript-unraveled/
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (ns good-fruit.core)
 
+;; Defining a data structure that will represent our fruit, including whether that fruit is rotten or clean.
+
+;; We have two collections of grapes, one green, one black.  Each cluster has 2 grapes on it (not a very big cluster in this example)
+
+(def grape-clusters
+  [{:grapes [{:rotten? false :clean? false}
+             {:rotten? true  :clean? false}]
+    :colour :green}
+   {:grapes [{:rotten? true  :clean? false}
+             {:rotten? false :clean? false}]}
+   :colour :black])
 
